7d43773b664cfbbe9815c0a033488c52c698ed19
Tom LePage <thomaslepage@live.com>
2016-03-10 14:21:21 +0000

    Added styling to home page

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'pages#home'
 
-  get 'pages/about', to: 'pages#about'
+  get 'about', to: 'pages#about'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
